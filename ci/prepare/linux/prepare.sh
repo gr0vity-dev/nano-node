060f1d8dbe60445e482dcc98c1f901bd3bab8e36
gr0vity <gr0vity.dev@gmail.com>
2024-10-09 11:29:24 +0200

    finnd lib

--- ci/prepare/linux/prepare.sh
+++ ci/prepare/linux/prepare.sh
@@ -5,19 +5,19 @@ COMPILER=${COMPILER:-gcc}
 echo "Compiler: '${COMPILER}'"
 
 # Function to install libbacktrace
-install_libbacktrace() {
-    echo "Installing libbacktrace..."
-    git clone https://github.com/ianlancetaylor/libbacktrace.git
-    cd libbacktrace
-    mkdir build
-    cd build
-    ../configure
-    make
-    sudo make install
-    cd ../..
-    rm -rf libbacktrace
-    echo "libbacktrace installation completed."
-}
+# install_libbacktrace() {
+#     echo "Installing libbacktrace..."
+#     git clone https://github.com/ianlancetaylor/libbacktrace.git
+#     cd libbacktrace
+#     mkdir build
+#     cd build
+#     ../configure
+#     make
+#     sudo make install
+#     cd ../..
+#     rm -rf libbacktrace
+#     echo "libbacktrace installation completed."
+# }
 
 # Common dependencies needed for building & testing
 DEBIAN_FRONTEND=noninteractive apt-get update -qq
@@ -38,7 +38,7 @@ valgrind \
 xorg xvfb xauth xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic
 
 # Install libbacktrace
-install_libbacktrace
+# install_libbacktrace
 
 # Compiler specific setup
 $(dirname "$BASH_SOURCE")/prepare-${COMPILER}.sh
