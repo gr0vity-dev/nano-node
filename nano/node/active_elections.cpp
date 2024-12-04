33cd2bfae0311f1bdaca917cc0b9e19cbc94c746
gr0vity <gr0vity.dev@gmail.com>
2024-12-04 11:00:41 +0100

    erase election immediately on confirmation
    We need to keep teh current cleanup loop, because elctions can timeout / be cancelled / ...

--- nano/node/active_elections.cpp
+++ nano/node/active_elections.cpp
@@ -408,7 +408,11 @@ nano::election_insertion_result nano::active_elections::insert (std::shared_ptr<
 				// Representative is defined as online if replying to live votes or rep_crawler queries
 				node.online_reps.observe (rep_a);
 			};
-			result.election = nano::make_shared<nano::election> (node, block_a, nullptr, observe_rep_cb, election_behavior_a);
+			auto confirmation_action_l = [this, root = root] (std::shared_ptr<nano::block> const & winner_a) {
+				// Immediately erase the election when confirmed
+				erase (root);
+			};
+			result.election = nano::make_shared<nano::election> (node, block_a, confirmation_action_l, observe_rep_cb, election_behavior_a);
 			roots.get<tag_root> ().emplace (entry{ root, result.election, std::move (erased_callback_a) });
 			node.vote_router.connect (hash, result.election);
 
